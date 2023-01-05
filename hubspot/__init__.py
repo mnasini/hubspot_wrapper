0f968653742978c20505cd89eecad07df4597ba9
mnasini <michele.nasini98@gmail.com>
2023-01-05 18:26:10 +0100

    created readme and general refactoring

--- hubspot/__init__.py
+++ hubspot/__init__.py
@@ -1,1 +1,15 @@
+import os
+import requests
+
+token = os.environ.get('HUBSPOT_TOKEN', None)
+
+if token is None:
+    raise Exception("invalid api key, see https://developers.hubspot.com/docs/api/crm/contacts for informations"
+    )
+session = requests.Session()
+session.params = {}
+session.params['token'] = token
+
+
+
 from .hubWrapper import hubWrapper
--- hubspot/hubWrapper.py
+++ hubspot/hubWrapper.py
@@ -1,2 +1,3 @@
 class hubWrapper(object):
-    pass
+    def __init__(self):
+        pass
