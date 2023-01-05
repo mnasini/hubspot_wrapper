f9aefd45013469051a00ae390c02f434661f1fdb
mnasini <michele.nasini98@gmail.com>
2023-01-05 20:00:55 +0100

    first test of the wrapper

--- hubspot/__init__.py
+++ hubspot/__init__.py
@@ -1,13 +1,14 @@
 import os
 import requests
-
-token = os.environ.get('HUBSPOT_TOKEN', None)
-
+#get the token as environment variable
+token = os.environ.get('token', None)
+#check if a token is present(by default for the sake of the exercise i inserted it in the env file)
 if token is None:
     raise Exception("invalid api key, see https://developers.hubspot.com/docs/api/crm/contacts for informations"
     )
 session = requests.Session()
 session.params = {}
+#save the token in a session variable to be always available for each api call
 session.params['token'] = token
 
 
--- hubspot/hubWrapper.py
+++ hubspot/hubWrapper.py
@@ -1,3 +1,11 @@
-class hubWrapper(object):
+from . import session
+
+class hubWrapper:
     def __init__(self):
+        self.token=session.params['token']
         pass
+    def info(self):
+        path= 'https://api.hubapi.com/crm/v3/objects/contacts?limit=10&archived=false'
+        session.headers.update({"Authorization": "Bearer {}".format(self.token)})
+        response = session.get(path)
+        return response.json()
