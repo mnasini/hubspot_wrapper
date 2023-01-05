4a99ea11d5d41546a5a97c25428a6213e9f67c3d
mnasini <michele.nasini98@gmail.com>
2023-01-05 22:58:16 +0100

    created first endpoint

--- hubspot/hubWrapper.py
+++ hubspot/hubWrapper.py
@@ -4,8 +4,9 @@ class hubWrapper:
     def __init__(self):
         self.token=session.params['token']
         pass
-    def info(self):
-        path= 'https://api.hubapi.com/crm/v3/objects/contacts?limit=10&archived=false'
+    def get_all_ids(self):     #add params list (to be apended for custom getter)
+        query_params = {'limit': '10', 'archieved':'false','properties':'email'}
+        path= 'https://api.hubapi.com/crm/v3/objects/contacts'
         session.headers.update({"Authorization": "Bearer {}".format(self.token)})
-        response = session.get(path)
+        response = session.get(path,params=query_params)
         return response.json()
