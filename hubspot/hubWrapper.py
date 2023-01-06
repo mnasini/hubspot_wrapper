8a1f38310f73917408bf4fcf819c7657a0474fa9
mnasini <michele.nasini98@gmail.com>
2023-01-06 01:10:49 +0100

    fixed get all ids and implemented get single id

--- hubspot/hubWrapper.py
+++ hubspot/hubWrapper.py
@@ -9,4 +9,18 @@ class hubWrapper:
         path= 'https://api.hubapi.com/crm/v3/objects/contacts'
         session.headers.update({"Authorization": "Bearer {}".format(self.token)})
         response = session.get(path,params=query_params)
+        response_dict=response.json()["results"]
+        return list(map(lambda x: x["email"],list(map(lambda x: x["properties"],response_dict))))
+
+        
+    def get_customer(self,customer_id,params):
+        query_params = {'limit': '10', 'archieved':'false','idProperty':'email','properties':params}
+        if params == "all":
+           query_params = {'limit': '10', 'archieved':'false','idProperty':'email'} 
+        path= 'https://api.hubapi.com/crm/v3/objects/contacts/{}'.format(customer_id)
+        session.headers.update({"Authorization": "Bearer {}".format(self.token)})
+        response = session.get(path,params=query_params)
         return response.json()
+    def get_customer_batch(self):
+        pass
+    #to be implemented
